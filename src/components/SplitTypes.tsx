6451ef5668473238d4eca54ee57fdf86ccc68334
shermaine sng <shersmallsng@gmail.com>
2024-03-20 00:13:19 +0800

    added calculation for isowed and owes

--- src/components/SplitTypes.tsx
+++ src/components/SplitTypes.tsx
@@ -4,8 +4,8 @@ import { RadioButton } from 'react-native-paper';
 import { Stack, useRouter } from 'expo-router';
 import { User, Expense, ExpenseMember} from '../constants/type';
 import { EvilIcons, Foundation, Ionicons } from "@expo/vector-icons";
-import { useQuery, gql, useMutation } from '@apollo/client';
-import { ADD_EXPENSE } from '../api/expenses';
+import { useMutation } from '@apollo/client';
+import { ADD_EXPENSE, ADD_EXPENSEMEMBER } from '../api/expenses';
 import { useAuth } from '../providers/AuthProvider';
 
 type SplitTypes = { 
@@ -21,20 +21,48 @@ export default function SplitTypes ({setShowSplitTypes, amount, participants, de
     const userId: string = session?.user.id !== undefined ? session.user.id.toString() : '';
     // const [payer, setPayer] = useState<User>(participants[0])
     const [payerIndex, setPayerIndex] = useState<number>(0)
-    const [percentages, setPercentages] = useState<number[]>([])
-    // const [newExpense, setNewExpense] = useState<Expense>()
+    const [percentagesToSplitCost, setPercentagesToSplitCost] = useState<number[]>([])
+    const [isOwed, setIsOwed] = useState<number[]>(Array(participants.length).fill(0));
+    const [owes, setOwes] = useState<number[]>(Array(participants.length).fill(0));
+    const [isOwedUpdated, setIsOwedUpdated] = useState<boolean>(false);
+    const [owesUpdated, setOwesUpdated] = useState<boolean>(false);
     const [expenseId, setExpenseId] = useState<number>()
-    const [expenseInput, setNewExpenseInput] = useState<ExpenseMember>()
-   const [addExpense, { data, loading, error }] = useMutation(ADD_EXPENSE)
+//    const [addExpense, { data, loading, error }] = useApolloMutation(ADD_EXPENSE)
+    const [addExpense, {loading, error}] = useMutation(ADD_EXPENSE, {
+        update(cache, {data: {addExpense}}) {
+            console.log("newly added expense", addExpense)
+            setExpenseId(parseInt(addExpense.id))
+        }
+    } )
+
+    const [addExpenseMember] = useMutation(ADD_EXPENSEMEMBER)
+
+    useEffect(() => {
+        if (expenseId) {
+            // Construct an array of ExpenseMemberInput objects
+            const expenseMemberInput = participants.map((participant) => ({
+                expense_id: expenseId,
+                member_id: participant.id,
+                isOwed: participant.isOwed,
+                owes: participant.owes
+            }));
+
+            // Call addExpenseMember mutation with the constructed input
+            addExpenseMember({
+                variables: {
+                    input: expenseMemberInput
+                }
+            });
+        }
+    }, [expenseId]);
 
    const router = useRouter();
 
-    //array of percentages when page first loads
+    //set array of percentages when page first loads
     useEffect(() => {
         const initialPercentages = Array(participants.length).fill(50)
-        setPercentages(initialPercentages)
-        // console.log(percentages)
-    }, [])
+        setPercentagesToSplitCost(initialPercentages)
+    }, [participants])
 
     // navigate to friends tab once expense submitted 
     // useEffect(() => {
@@ -46,45 +74,64 @@ export default function SplitTypes ({setShowSplitTypes, amount, participants, de
 
     //take in string and parse it to number to store in percentage array
     const handlePercentageChange = (text:string, index:number) => {
-        const newPercentages = [...percentages];
+        const newPercentages = [...percentagesToSplitCost];
         if (text === '') {
             newPercentages[index] = 0; // Set a default value when input is empty
         } else {
             newPercentages[index] = parseFloat(text);
         }
-        setPercentages(newPercentages)
+        setPercentagesToSplitCost(newPercentages)
     }
 
     //calculate amount each person owes/isOwed
     const calculateAmountOwed = () => {
-        const payerPercentage = percentages[payerIndex];
-        const payerAmount = (payerPercentage / 100) * amount; //5
-        const remainingAmount = amount - payerAmount; //5
+        //go through each percentage and index, if index is payer index,
+        //calculate isOwed through percentage and add to array 
+        percentagesToSplitCost.forEach((splitPercentage, index) => {
+            const share = parseFloat(((splitPercentage / 100) * amount).toFixed(2));//calc each participant's share
 
-        const owes = participants.map((participant, index) => {
             if (index === payerIndex) {
-                return 0; // Payer owes nothing to themselves
+                setIsOwed((prevIsOwed) => {
+                   const updatedIsOwed = [...prevIsOwed]
+                   updatedIsOwed[index] = share
+                   return updatedIsOwed
+                })
+                setIsOwedUpdated(true);
+            }else {
+                setOwes(prevOwes => {
+                    const updatedOwes = [...prevOwes]
+                    updatedOwes[index] = share
+                    return updatedOwes
+                })
+                setOwesUpdated(true);
             }
-            const participantPercentage = percentages[index];
-            const participantAmount = (participantPercentage / 100) * remainingAmount;
-            return participantAmount;
-        });
+        })
     }
+
     //make sure the percentages add up to 100. if yes, add expense
     //once expense is added in expense table, add in expense_member table for each member 
     const validatePercentages = () => {
-        console.log("submit button clicked")
-        const totalPercentage = percentages.reduce((sum, percentage) => sum + percentage, 0);
+        const totalPercentage = percentagesToSplitCost.reduce((sum, percentage) => sum + percentage, 0);
         if (totalPercentage !== 100) {
             Alert.alert('Total percentage must be 100.');
         } else {
+            calculateAmountOwed()
             addingExpense()
         }
     }
 
+    useEffect(() => {
+        // Check if both isOwed and owes have been updated
+        if (isOwedUpdated && owesUpdated) {
+            console.log(isOwed)
+            addingExpense(); // Call addingExpense only if both are updated
+            setIsOwedUpdated(false); // Reset flags
+            setOwesUpdated(false);
+        }
+    }, [isOwedUpdated, owesUpdated]); // Run effect whenever flags change
+
     const addingExpense = async () => {
-        console.log("in adding expense")
-        // console.log(participants[payerIndex].id, amount, description)
+        console.log("in adding expense")        
         try {
             await addExpense({
                 variables: {
@@ -96,10 +143,6 @@ export default function SplitTypes ({setShowSplitTypes, amount, participants, de
                     }
                 }
             });
-            if (data && data.addExpense) {
-                console.log(data.addExpense)
-                setExpenseId(parseInt(data.addExpense.id))
-            }
             //once added to Expense table, also add to Expense_members table - send in as array of expense members 
         } catch (error) {
             console.error("Error adding expense:", error);
@@ -155,9 +198,9 @@ export default function SplitTypes ({setShowSplitTypes, amount, participants, de
                 {participants && participants.length>0 && participants.map((participant, index) => (
                     <View key={index} className='flex-row items-center justify-between'>
                         <Text>{participant.username}</Text>
-                        {percentages && percentages.length > 0 && (
+                        {percentagesToSplitCost && percentagesToSplitCost.length > 0 && (
                             <View className='flex-row items-center'>
-                                <TextInput className="border-b-[1px] text-lg text-black pb-2" value={percentages[index].toString()} onChangeText={(text) => handlePercentageChange(text, index)} placeholderTextColor="gray" keyboardType="numeric"/>
+                                <TextInput className="border-b-[1px] text-lg text-black pb-2" value={percentagesToSplitCost[index].toString()} onChangeText={(text) => handlePercentageChange(text, index)} placeholderTextColor="gray" keyboardType="numeric"/>
                                 <Text>%</Text>
                             </View>
                          )
