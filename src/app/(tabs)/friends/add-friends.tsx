4e451e00bf91ab64b1d054d3ecc6c6d245b88510
shermaine sng <shersmallsng@gmail.com>
2024-03-27 23:59:02 +0800

    complete new expense feature

--- src/app/(tabs)/friends/add-friends.tsx
+++ src/app/(tabs)/friends/add-friends.tsx
@@ -1,43 +1,96 @@
-import { View, Text, TouchableOpacity, ScrollView, SafeAreaView, Pressable, StyleSheet} from "react-native";
+import { View, Text, TouchableOpacity, ScrollView, SafeAreaView, Pressable, StyleSheet, Alert} from "react-native";
 import React, {useEffect, useState} from "react";
 import { Stack } from "expo-router";
 import { Ionicons, AntDesign} from "@expo/vector-icons";
 import * as Contacts from 'expo-contacts'
-import { useGetContactsWithAccounts } from "@/src/api/friends";
+// import { useGetContactsWithAccounts } from "@/src/api/friends";
 import { User } from "@/src/constants/type";
-
+import { ADD_FRIEND, GET_PROFILES_WITH_PHONE_NUMBERS } from "@/src/api/friends";
+import { useQuery, useMutation } from '@apollo/client';
+import { useAuth } from "@/src/providers/AuthProvider";
 
 export default function AddFriendsScreen() {
-  const phones:string[] = []
-  const  {data: existingPhones, error} = useGetContactsWithAccounts()
-  const [contacts, setContacts] = useState<User[] >([])
+  const {session} = useAuth()
+  const userId:string = session?.user.id
+  const [contacts, setContacts] = useState<User[] | null>([])
 
+  const {loading: getProfilesLoading, error: getProfilesError, data: getProfiles} = useQuery(GET_PROFILES_WITH_PHONE_NUMBERS);
+  const [addFriend, {loading, error:addFriendError}] = useMutation(ADD_FRIEND)
 
+  //when component first loads, render list of contacts that have an account
   useEffect(() => {
-    (async () => {
-      const { status } = await Contacts.requestPermissionsAsync();
-      if (status === 'granted') {
-        const { data } = await Contacts.getContactsAsync({
-          fields: [Contacts.Fields.PhoneNumbers],
-        });
+    const fetchData = async () => {
+        try {
+            const { status } = await Contacts.requestPermissionsAsync();
+            if (status === 'granted') {
+                const { data } = await Contacts.getContactsAsync({
+                    fields: [Contacts.Fields.PhoneNumbers],
+                });
 
-        if (data.length > 0) {
-          data.forEach(contact => contact.phoneNumbers && phones.push(contact.phoneNumbers[0].number as string))
-          
-          const filteredContacts = existingPhones?.filter(user => {
-            return phones.some(phone => normalizePhoneNumber(user.phone_number).includes(normalizePhoneNumber(phone)));
-          });
-          // console.log(filteredContacts)
-          // console.log(phones)
-          if (!error) setContacts(filteredContacts)
+                if (data.length > 0) {
+                    const phones:string[] = []
+                    data.forEach(contact => contact.phoneNumbers && phones.push(contact.phoneNumbers[0].number as string));
+                    
+                    let filteredContacts: User[] = []
+                    if (getProfiles && getProfiles.profiles) {
+                      filteredContacts = getProfiles.profiles?.filter((user: User) => {
+                          return phones.some(phone => {
+                              const normalizedUserPhone = normalizePhoneNumber(user.phone_number);
+                              const normalizedContactPhone = normalizePhoneNumber(phone);
+                              return normalizedUserPhone === normalizedContactPhone;
+                          });
+                      }) || [];
+                    }
+                    
+                    if (!getProfilesError) {
+                        console.log(filteredContacts)
+                        setContacts(filteredContacts);
+                    }
+                }
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+    fetchData();
+}, [getProfiles, getProfilesError]);
+  
+  function normalizePhoneNumber(phoneNumber: string): string {
+    // Remove non-digit characters from phone number
+    const digitsOnly = phoneNumber.replace(/\D/g, '');
+
+    // If the phone number starts with a country code, remove it
+    if (digitsOnly.length > 8 && digitsOnly.startsWith('65')) {
+        return digitsOnly.slice(2); // Remove '65'
+    }
+    
+    return digitsOnly;
+}
+
+  const handleAddFriend = async(contactId:String) => {
+    console.log('in handleAddFriend', userId, contactId)
+    try {
+      const response = await addFriend({
+        variables: {
+          input: {
+            "user_id": userId,
+            "friend_id": contactId, 
+            "status": "confirmed"
           }
-      }
-    })();
-  }, []);
-
-  function normalizePhoneNumber(phoneNumber:string) {
-    // Remove non-numeric characters from the phone number
-    return phoneNumber.replace(/\D/g, '');
+        }
+      })
+      if (response && response.data.addFriend === true) {
+        console.log("Friend added successfully!", response.data);
+        Alert.alert("Friend has been added!")
+    } else {
+        console.error("Mutation may not have been successful:", response.data);
+    }
+    } catch(err) {
+      console.error("Error adding expense:", addFriendError);
+    }
+    if (addFriendError) {
+      console.error("GraphQL Error:", addFriendError);
+    }
   }
 
   return (
@@ -57,9 +110,17 @@ export default function AddFriendsScreen() {
         <Text style={{paddingBottom:10}}>From your contacts:</Text>
         {contacts && contacts.length > 0 &&
           contacts.map(contact => (
-            <View>
-              <Text className="text-base ">{contact.username}</Text>
-              <Text>{contact.phone_number}</Text>
+            <View className='flex-row justify-between pb-2' key={contact.id}>
+              <View>
+                <Text className="text-base">{contact.username}</Text>
+                <Text>{contact.phone_number}</Text>
+              </View>
+              <View>
+                {/* <Button text='Add as friend'></Button> */}
+                <TouchableOpacity onPress={() => handleAddFriend(contact.id)} style={styles.container}>
+                  <Text>Add as friend</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           ))}
       </ScrollView>
@@ -73,5 +134,17 @@ const styles = StyleSheet.create({
     alignItems:'center', 
     justifyContent:'center',
     padding:10
-  }
+  },
+    container: {
+      backgroundColor:'#FDD7E9',
+      padding: 10,
+      alignItems: 'center',
+      borderRadius: 100,
+      // marginVertical: 10,
+    },
+    text: {
+      fontSize: 16,
+      fontWeight: '600',
+      color: 'white',
+    },
 })
